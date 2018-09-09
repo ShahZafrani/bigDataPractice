2ce75ef7fbde5fa83cb6b152f91330fa301643c3
Shah Zafrani <shahzafrani@gmail.com>
2018-09-09 14:14:30 -0400

    working entropy, working split. next up is info gain

--- decisionTree/decisionTree.py
+++ decisionTree/decisionTree.py
@@ -7,51 +7,50 @@
 #
 
 import numpy as np
+import math
 
 training = np.genfromtxt('car.training.csv', dtype=None, encoding="utf-8", delimiter=',')
 print("training shape: {}, {}".format(training.shape[0], training.shape[1]))
 test = np.genfromtxt('car.test.csv', dtype=None, encoding="utf-8", delimiter=',')
 print("test shape: {}, {}".format(test.shape[0], test.shape[1]))
 
-def featureAttributeOccurance(attr, featCol, data):
-    count = 0
-    for d in range(len(data[:,featCol])):
-        if data[d, featCol] == attr:
-            if data[d, -1] == 'acc':
-                count+=1
-    return count, sum(data[:,featCol]==attr) - count
-
 def accuracy(preds, labels):
     return float(sum(preds == labels[:,-1])) / len(labels)
 
-def trainDecisionTree():
-    return "hoopla"
-def giniIndex(dataset):
-    #assuming labels are at the end
-    labels = dataset[:,-1]
-    uniq_classes = set(labels)
-    return uniq_classes
+def entropy(dataSplit): # formatted as an array of counts: [5,9]
+    total = sum(dataSplit)
+    numClasses = len(dataSplit)
+    entropy = 0
+    for d in dataSplit:
+        prob = float(d) / total
+        entropy += -1 * (prob * math.log(prob, 2))
+    return entropy
 
-def entropy(labeledData): # this is assuming the labels are in the last column
-    # Entropy :
+def countSplit(labeledData): # this is assuming the labels are in the last column
+    # Entropy : summation(C[], i): -1 * probability(C[i]) * log2(probability(C[i]))
+    labeledData = np.array(labeledData)
     totalSamples = len(labeledData[:,-1])
-    return 0.5
+    uniqueClasses = set(labeledData[:,-1])
+    dataSplit = []
+    for u in uniqueClasses:
+        dataSplit.append(sum(labeledData[:,-1]==u))
+    return dataSplit
 
-# class DecisionTree:
-#     def __init__(self, featCol, featAttr, leftTree, rightTree):
-#         self.featCol = featCol
-#         self.featAttr = featAttr
-#         self.leftTree = leftTree
-#         self.rightTree = rightTree
-#
-# def bruteForceSplit(data): # greedy strategy that locally optimizes
-#
-#
-# def trainTree(): # based on Hunt's Algorithm
+def performSplit(labeledData, featCol, featVal):
+    matchBranch = []
+    notMatchBranch = []
+    for d in labeledData:
+        if d[featCol] == featVal:
+            matchBranch.append(d)
+        else:
+            notMatchBranch.append(d)
+    return matchBranch, notMatchBranch
 
-
-
-
+print("starting entropy of training set: \n\t" + str(entropy(countSplit(training))))
+print("if split on col1=\"vhigh\":")
+vhigh, nvhigh = performSplit(training, 0, "vhigh")
+print("vhigh:\n\t" + str(entropy(countSplit(vhigh))))
+print("not vhigh:\n\t" + str(entropy(countSplit(nvhigh))))
 
 
 
