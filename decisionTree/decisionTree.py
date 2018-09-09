7dfd6a808a88e4364ac2117cac068af5c2e82839
Shah Zafrani <shahzafrani@gmail.com>
2018-09-09 13:27:41 -0400

    got some direction on the project

--- decisionTree/decisionTree.py
+++ decisionTree/decisionTree.py
@@ -1,25 +1,72 @@
 # python 3.6
 # decisionTree.py
 
+# Resources:
+#   - http://saedsayad.com/decision_tree.htm
+#      - Used for help understanding entropy split and information gain
+#
+
 import numpy as np
 
-training = np.genfromtxt('car.training.csv', dtype=None, delimiter=',')
-print("training shape: ",  training.shape)
-test = np.genfromtxt('car.test.csv', dtype=None, delimiter=',')
-print("test shape: ",  test.shape)
+training = np.genfromtxt('car.training.csv', dtype=None, encoding="utf-8", delimiter=',')
+print("training shape: {}, {}".format(training.shape[0], training.shape[1]))
+test = np.genfromtxt('car.test.csv', dtype=None, encoding="utf-8", delimiter=',')
+print("test shape: {}, {}".format(test.shape[0], test.shape[1]))
 
-stupid_prediction = training[0, -1]
-
-print(stupid_prediction)
-predictions = []
-for i in range(test.shape[0]):
-    predictions.append(stupid_prediction)
+def featureAttributeOccurance(attr, featCol, data):
+    count = 0
+    for d in range(len(data[:,featCol])):
+        if data[d, featCol] == attr:
+            if data[d, -1] == 'acc':
+                count+=1
+    return count, sum(data[:,featCol]==attr) - count
 
 def accuracy(preds, labels):
     return float(sum(preds == labels[:,-1])) / len(labels)
 
 def trainDecisionTree():
     return "hoopla"
+def giniIndex(dataset):
+    #assuming labels are at the end
+    labels = dataset[:,-1]
+    uniq_classes = set(labels)
+    return uniq_classes
+
+def entropy(labeledData): # this is assuming the labels are in the last column
+    # Entropy :
+    totalSamples = len(labeledData[:,-1])
+    return 0.5
+
+# class DecisionTree:
+#     def __init__(self, featCol, featAttr, leftTree, rightTree):
+#         self.featCol = featCol
+#         self.featAttr = featAttr
+#         self.leftTree = leftTree
+#         self.rightTree = rightTree
+#
+# def bruteForceSplit(data): # greedy strategy that locally optimizes
+#
+#
+# def trainTree(): # based on Hunt's Algorithm
 
 
-print(accuracy(predictions, test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #
